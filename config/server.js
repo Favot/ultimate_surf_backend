e35c911dbf833fe09ee3c15eb731645da58e9954
Favot <70208301+Favot@users.noreply.github.com>
2021-05-27 14:51:40 +0200

    init strapi for ultimate_surf_2000

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd4522f000d8bb07bf875021cba93fcf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ee5d8eae-2594-44e9-97e3-5854ea6a295c'
+};
